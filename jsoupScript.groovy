de9e4926c3d79f94c2bfcc28e62020d55a0792c2
Roberto Guerra <uris77@gmail.com>
2013-02-20 10:18:00 -0600

    Removed test code

--- jsoupScript.groovy
+++ jsoupScript.groovy
@@ -3,29 +3,6 @@
 @Grab(group='commons-lang', module='commons-lang', version='2.4')
 ])
 import org.apache.commons.lang.StringEscapeUtils
-/*
-org.jsoup.nodes.Document portrait = org.jsoup.Jsoup.connect("http://localhost:3000/slab?eventName=MarionetteJS%20is%20just%20freaking%20Awesome&screenSize=320x356").get()
-org.jsoup.select.Elements portriatElement = portrait.select('#slider ul > li')
-println "\n\n############ PORTRIAT ########################"
-println "portriat Element: "
-println portriatElement
-org.jsoup.nodes.Document landscape = org.jsoup.Jsoup.connect("http://localhost:3000/slab?eventName=MarionetteJS%20is%20just%20freaking%20Awesome&screenSize=480x208").get()
-org.jsoup.select.Elements landscapeElement = landscape.select('#slider ul > li')
-println "\n\n############ LANDSCAPE ########################"
-println "landscape Element: "
-println landscapeElement
-org.jsoup.nodes.Document doc = org.jsoup.Jsoup.connect("http://localhost:3000/slab?eventName=MarionetteJS%20is%20just%20freaking%20Awesome").get()
-org.jsoup.select.Elements slider = doc.select('#slider ul > li')
-println "\n\n############ DEFAULT ########################"
-println "slider: "
-println slider.first()
-org.jsoup.select.Elements landscapeEl = doc.select('#480x208 ul > li')
-println "\n\nlandscape: "
-println landscapeEl.first()
-org.jsoup.select.Elements portriatEl = doc.select('#320x356 ul > li')
-println "\n\nportrait: "
-println portriatEl.first()
-*/
 //String url = "http://localhost:8080/uppercut/crawl?eventName="
 String name = "Marionettejs is Just Freaking AweSome"
 //String host = "dev.chicago.com.local:3001"
